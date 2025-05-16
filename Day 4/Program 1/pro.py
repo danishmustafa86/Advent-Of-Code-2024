dc7b1c764d3a166f9cb1661ac9d74fa25030ed5a
unknown <danishjajja86@gmail.com>
2025-05-16 12:54:54 +0500

    Day 4 practice

--- Day 4/Program 1/pro.py
+++ Day 4/Program 1/pro.py
@@ -6,8 +6,8 @@ def count_xmas(grid):
     directions = [
         (0, 1), (0, -1),  # Horizontal
         (1, 0), (-1, 0),  # Vertical
-        (1, 1), (-1, -1), # Diagonal (top-left  bottom-right)
-        (1, -1), (-1, 1)  # Diagonal (top-rig bottom-left)
+        (1, 1), (-1, -1), # Diagonal (top-left to bottom-right)
+        (1, -1), (-1, 1)  # Diagonal (top-right to bottom-left)
     ]
     
     count = 0
